b04a115982a090f0a781e81b7e51c1fe731b7f9e
panchen <panchen@tigerbrokers.com>
2021-09-03 14:16:52 +0800

    PinView.

---
+++ pinview-library/src/main/java/me/bytebeats/views/pinview/InputType.kt
@@ -0,0 +1,14 @@
+package me.bytebeats.views.pinview
+
+/**
+ * @Author bytebeats
+ * @Email <happychinapc@gmail.com>
+ * @Github https://github.com/bytebeats
+ * @Created on 2021/9/3 10:38
+ * @Version 1.0
+ * @Description TO-DO
+ */
+
+enum class InputType {
+    TEXT, NUMBER
+}
